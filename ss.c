a1fb9cea83f83752e8c857604b5a3a307dd22117
doruison <doruison@hotmail.com>
2015-03-20 11:04:08 +0800

    修正标点

--- ss.c
+++ ss.c
@@ -8,8 +8,8 @@ int sockfd,len;
 struct sockaddr_in addr;
 char buffer[256];
 /*建立socket*/
-if(sockfd=socket (AF_INET,SOCK_DGRAM,0))<0){
-perror (“socket”);
+if((sockfd=socket(AF_INET,SOCK_DGRAM,0))<0){
+perror ("socket");
 exit(1);
 }
 /*填写sockaddr_in 结构*/
@@ -18,14 +18,14 @@ addr.sin_family=AF_INET;
 addr.sin_port=htons(PORT);
 addr.sin_addr=hton1(INADDR_ANY) ;
 if (bind(sockfd, &addr, sizeof(addr))<0){
-perror(“connect”);
+perror("connect");
 exit(1);
 }
 while(1){
 bezro(buffer,sizeof(buffer));
 len = recvfrom(socket,buffer,sizeof(buffer), 0 , &addr &addr_len);
 /*显示client端的网络地址*/
-printf(“receive from %s\n “ , inet_ntoa( addr.sin_addr));
+printf("receive from %s\n " , inet_ntoa( addr.sin_addr));
 /*将字串返回给client端*/
 sendto(sockfd,buffer,len,0,&addr,addr_len);”
 }
